e3f77b43ecc1ee9f559fe8b6a41dd259921c53ed
Andrei Antonescu <andreibantonescu@gmail.com>
2014-02-07 13:10:10 +0000

    walks well along the wall

--- sonar_wall.py
+++ sonar_wall.py
@@ -2,11 +2,11 @@ from utils import *
 
 # Put the sensor on the right side
 # 0 for right, 1 for left
-side = 1
+side = 0
 mag = 15
 print 'The sensor needs to be placed on the', 'left' if side else 'right', 'side!'
 
-speed = 140
+speed = 100
 
 s3 = PORT_3
 enableSensor(s3, TYPE_SENSOR_ULTRASONIC_CONT)
@@ -36,14 +36,18 @@ def keep_wall():
   for i in xrange(0, 10):
     update()
     vals.append( sensor(s3) )
-  
+  '''
   # read initial value
   normal = median(vals)   
+  '''
+  normal = 30
   lastDif = normal
   changeTime = 0
+  trackOverTurn = 0
 
   while True:
     changeTime += 1
+    
     # We don't want update to move motors
 
     # read 5 times sonar sensor
@@ -56,13 +60,24 @@ def keep_wall():
     # this way we discard spike values
     dif = most_common(vals)
     
-    if dif is 0 or mod(dif) < mod(lastDif) or changeTime < 20:
-      spds = [speed, speed]
+    if trackOverTurn < 30:
+      if dif is 0 or mod(dif) < mod(lastDif) or changeTime < 7:
+        spds = [speed, speed]
+      else:
+        changeTime = 0
+        spds = calib(speed, dif - normal)
+        if dif > 30:
+          trackOverTurn += 1
+        else:
+          trackOverTurn = 0
+      print 'spds', spds, 'dif', dif, 'dif - normal', dif - normal  
     else:
-      changeTime = 0
-      spds = calib(speed, dif - normal)
-    print 'spds', spds, 'dif', dif - normal  
-
+      trackOverTurn = 0
+      print 'else code'
+      setSpeed(m1, spds[1 - side] + 15)
+      setSpeed(m2, spds[side] - 15)
+      
+    
     # move with the desired calibrated speed
     if side is 1:
       setSpeed(m1, spds[0])
